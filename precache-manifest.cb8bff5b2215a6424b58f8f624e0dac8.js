7f744f5fe5e16ac289093e624fcc525f303892de
ntaparia <taparia.nikhil@gmail.com>
2022-05-13 02:54:53 -0400

    Updates

---
+++ precache-manifest.cb8bff5b2215a6424b58f8f624e0dac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a575965b5953418bec19",
+    "url": "/Final_Project/static/css/main.435ee3c4.chunk.css"
+  },
+  {
+    "revision": "a575965b5953418bec19",
+    "url": "/Final_Project/static/js/main.a575965b.chunk.js"
+  },
+  {
+    "revision": "ea04ef38693dbe45bdf8",
+    "url": "/Final_Project/static/js/1.ea04ef38.chunk.js"
+  },
+  {
+    "revision": "19102bc14247c98bd7fe",
+    "url": "/Final_Project/static/js/runtime~main.19102bc1.js"
+  },
+  {
+    "revision": "f5dece577effb4e54d6f57b0781b6fe2",
+    "url": "/Final_Project/index.html"
+  }
+];
